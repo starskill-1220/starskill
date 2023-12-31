d0671215fba3b790703bfd273156dcbeeec1805e
starskill-1220 <contact@star-skill.jp>
2023-12-31 12:49:08 +0900

    header-white.phpをheader.phpと統合完了

--- header.php
+++ header.php
@@ -5,14 +5,21 @@
 <!--Bodyここから！！！！！！！！！！-->
   <body class="fade">
   <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9W2DRL"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9W2DRL"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
-  <header class="header-black">
-    <a class="logo header-logo" href="<?php echo home_url('/'); ?>">
-      <p data-text3="STARSKILL">STARSKILL</p>
-    </a>
-    <?php get_template_part('gNav') ?>
+      <header class="
+        <?php if(get_field("ヘッダーの色") == "white"): ?>
+          header-white
+        <?php else :?>
+          header-black
+        <?php endif ?>
+      ">
+        <a class="logo header-logo" href="<?php echo home_url('/'); ?>">
+          <p data-text3="STARSKILL">STARSKILL</p>
+        </a>
+        <?php get_template_part('gNav') ?>
 
-  </header>
+      </header>
+    
